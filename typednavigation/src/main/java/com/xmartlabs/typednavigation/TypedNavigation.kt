3090c8231f2ddd7129837fb67aa5c590f3531a34
Felipe <f7.deleon@gmail.com>
2021-10-05 10:19:10 -0300

    Added deep links

--- typednavigation/src/main/java/com/xmartlabs/typednavigation/TypedNavigation.kt
+++ typednavigation/src/main/java/com/xmartlabs/typednavigation/TypedNavigation.kt
@@ -1,6 +1,7 @@
 package com.xmartlabs.typednavigation
 
 import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavDeepLink
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
 
@@ -9,9 +10,11 @@ sealed class TypedNavigation {
     /** Implementation for the [TypedNavigationInterface0] */
     data class E(
         override val name: String,
-        override val url: String = "$name/",
-        override val navArguments: List<NamedNavArgument> = listOf()
+        override val deepLinks: List<NavDeepLink> = listOf(),
     ) : TypedNavigation(), TypedNavigationInterface0 {
+        override val url: String = "$name/"
+        override val navArguments: List<NamedNavArgument> = listOf()
+
         override fun route(): String = "$name/"
     }
 
@@ -19,11 +22,13 @@ sealed class TypedNavigation {
     data class A1<A>(
         override val name: String,
         override val t1: NavType<A>,
-        override val url: String = "$name/{a1}",
+        override val deepLinks: List<NavDeepLink> = listOf(),
+    ) : TypedNavigation(), TypedNavigationInterface1<A> {
+        override val url: String = "$name/{a1}"
         override val navArguments: List<NamedNavArgument> = listOf(
             navArgument("a1") { type = t1 }
-        ),
-    ) : TypedNavigation(), TypedNavigationInterface1<A> {
+        )
+
         override fun route(arg1: A): String = "$name/${arg1.toString()}"
     }
 
@@ -32,12 +37,15 @@ sealed class TypedNavigation {
         override val name: String,
         override val t1: NavType<A1>,
         override val t2: NavType<A2>,
-        override val url: String = "$name/{a1}/{a2}",
+        override val deepLinks: List<NavDeepLink> = listOf(),
+    ) : TypedNavigation(), TypedNavigationInterface2<A1, A2> {
+        override val url: String = "$name/{a1}/{a2}"
+
         override val navArguments: List<NamedNavArgument> = listOf(
             navArgument("a1") { type = t1 },
             navArgument("a2") { type = t2 }
-        ),
-    ) : TypedNavigation(), TypedNavigationInterface2<A1, A2> {
+        )
+
         override fun route(arg1: A1, arg2: A2): String =
             "$name/${arg1.toString()}/${arg2.toString()}"
     }
@@ -48,13 +56,15 @@ sealed class TypedNavigation {
         override val t1: NavType<A1>,
         override val t2: NavType<A2>,
         override val t3: NavType<A3>,
-        override val url: String = "$name/{a1}/{a2}/{a3}",
+        override val deepLinks: List<NavDeepLink> = listOf(),
+    ) : TypedNavigation(), TypedNavigationInterface3<A1, A2, A3> {
+        override val url: String = "$name/{a1}/{a2}/{a3}"
         override val navArguments: List<NamedNavArgument> = listOf(
             navArgument("a1") { type = t1 },
             navArgument("a2") { type = t2 },
             navArgument("a3") { type = t3 },
-        ),
-    ) : TypedNavigation(), TypedNavigationInterface3<A1, A2, A3> {
+        )
+
         override fun route(arg1: A1, arg2: A2, arg3: A3): String =
             "$name/${arg1.toString()}/${arg2.toString()}/${arg3.toString()}"
     }
@@ -66,14 +76,16 @@ sealed class TypedNavigation {
         override val t2: NavType<A2>,
         override val t3: NavType<A3>,
         override val t4: NavType<A4>,
-        override val url: String = "$name/{a1}/{a2}/{a3}/{a4}",
+        override val deepLinks: List<NavDeepLink> = listOf(),
+    ) : TypedNavigation(), TypedNavigationInterface4<A1, A2, A3, A4> {
+        override val url: String = "$name/{a1}/{a2}/{a3}/{a4}"
         override val navArguments: List<NamedNavArgument> = listOf(
             navArgument("a1") { type = t1 },
             navArgument("a2") { type = t2 },
             navArgument("a3") { type = t3 },
             navArgument("a4") { type = t4 },
-        ),
-    ) : TypedNavigation(), TypedNavigationInterface4<A1, A2, A3, A4> {
+        )
+
         override fun route(arg1: A1, arg2: A2, arg3: A3, arg4: A4): String =
             "$name/${arg1.toString()}/${arg2.toString()}/${arg3.toString()}/${arg4.toString()}"
     }
@@ -86,15 +98,18 @@ sealed class TypedNavigation {
         override val t3: NavType<A3>,
         override val t4: NavType<A4>,
         override val t5: NavType<A5>,
-        override val url: String = "$name/{a1}/{a2}/{a3}/{a4}/{a5}",
+        override val deepLinks: List<NavDeepLink> = listOf(),
+    ) : TypedNavigation(), TypedNavigationInterface5<A1, A2, A3, A4, A5> {
+        override val url: String = "$name/{a1}/{a2}/{a3}/{a4}/{a5}"
+
         override val navArguments: List<NamedNavArgument> = listOf(
             navArgument("a1") { type = t1 },
             navArgument("a2") { type = t2 },
             navArgument("a3") { type = t3 },
             navArgument("a4") { type = t4 },
             navArgument("a5") { type = t5 },
-        ),
-    ) : TypedNavigation(), TypedNavigationInterface5<A1, A2, A3, A4, A5> {
+        )
+
         override fun route(arg1: A1, arg2: A2, arg3: A3, arg4: A4, arg5: A5): String =
             "$name/${arg1.toString()}/${arg2.toString()}/${arg3.toString()}/${arg4.toString()}/${arg5.toString()}"
     }
